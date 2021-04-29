1418f3ce735f577c1b0060c5f1e8e103e212e7d9
BrendanGlancy <bglance68@gmail.com>
2021-04-29 15:18:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+to start the server for testing 
+php -S localhost:8000
+
+php -S for php server then the name of the link
+
+
+link
+http://localhost:8000
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Hello World</title>
+</head>
+<body>
+Hello, World
+<?php
+    // You can comment like this, single line comment
+    # or you can comment like this
+    /*
+     * This is how you do multiple line comments, same as Java 
+     */
+
+    # this file is saved as index so browsers automatically 
+    # open as the home page
+    echo "Test PHP";
+
+?>
+</body>
+
+</html>
